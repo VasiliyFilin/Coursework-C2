8e553ba26ade01170c3d78b2c6def8e2777e4bb3
Vasily Filin <pro.vasiliy.filin@gmail.com>
2024-03-11 19:29:11 +0300

    Coursework c2 Init

---
+++ src/main/java/skypro/courseworkc2/CourseworkC2Application.java
@@ -0,0 +1,13 @@
+package skypro.courseworkc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseworkC2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseworkC2Application.class, args);
+	}
+
+}
